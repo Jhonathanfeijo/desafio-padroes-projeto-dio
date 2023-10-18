64cd56d0953350163210ac55d8e3de76474e04dc
jhonathanfeijo <jhonathanfeijomoraes@gmail.com>
2023-10-18 02:44:18 -0400

    Projeto de padroes DIO 2023

---
+++ src/main/java/me/dio/domain/cliente/Cliente.java
@@ -0,0 +1,47 @@
+package me.dio.domain.cliente;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import me.dio.domain.endereco.Endereco;
+
+@Entity(name = "cliente")
+@Table(name = "Cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String nome;
+	@ManyToOne
+	private Endereco endereco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
